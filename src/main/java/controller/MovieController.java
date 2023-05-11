720877cd484e2a748ffd1d650b826683c796c7a0
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-05-11 20:08:35 +0200

    Adding logout function in movie fxml

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MovieController {
+    @FXML
+    private Menu Cart;
+    @FXML
+    private Menu Logout;
+    @FXML
+    private Menu Movies;
+    @FXML
+    void logout(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
