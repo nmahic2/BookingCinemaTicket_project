7c68b39afe1e30b8bd680b5eee21fc0c880a60f8
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-05-12 15:10:27 +0200

    Adding options in menu bar in movie scene 2

--- src/main/java/controller/MovieController.java
+++ src/main/java/controller/MovieController.java
@@ -4,15 +4,15 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class MovieController {
     @FXML
