e1b3068d71f82e5dc80f580cb0a79df007eb1d2d
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-05-22 20:39:40 +0200

    Changing Cart scene vol2

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -1,16 +1,18 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Menu;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class CartController {
     @FXML
@@ -19,7 +21,32 @@ public class CartController {
     private Menu Logout;
     @FXML
     private Menu Movies;
+    //tabela,i dodavanje elemenata
+    @FXML
+    private TextField numberOfTickets;
 
+    @FXML
+    private TableView<Data> tableView;
+
+    @FXML
+    private TableColumn<Data, String> Movie;
+
+    @FXML
+    private TableColumn<Data, String> DateAndTime;
+
+    @FXML
+    private TableColumn<Data, Integer> NumberOfTickets;
+
+    private int id = 0;
+
+    private ObservableList<Data> dataListMovieCart = FXCollections.observableArrayList();
+
+    //dugme za delete u update
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button addButton;
     @FXML
     void logout(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
@@ -85,4 +112,82 @@ public class CartController {
         stage.setScene(scene);
         stage.show();
     }
+
+    //************************
+
+
+    //tabela i dodavanje elemenata
+    public void addData(ActionEvent actionEvent) {
+
+        NumberOfTickets.setCellValueFactory(new PropertyValueFactory<>("Number of tickets"));
+        Movie.setCellValueFactory(new PropertyValueFactory<>("Movie"));
+        DateAndTime.setCellValueFactory(new PropertyValueFactory<>("Date and time"));
+
+        String dataB = Movie.getText();
+        String dataC = DateAndTime.getText();
+        int dataD = Integer.parseInt(NumberOfTickets.getText());
+
+        dataListMovieCart.add(new Data(dataD, dataB, dataC));
+        tableView.setItems(dataListMovieCart);
+
+       /* NumberOfTickets.clear();
+        Movie.clear();
+        DateAndTime.clear();
+        */
+    }
+
+
+    public class Data {
+        private int NumberOfTickets;
+        private String Movie;
+        private String DateAndTime;
+
+        public Data(int NumberOfTickets, String Movie, String DateAndTime) {
+            this.Movie = Movie;
+            this.NumberOfTickets = NumberOfTickets;
+            this.DateAndTime = DateAndTime;
+        }
+
+        public int getNumberOfTickets() {
+            return NumberOfTickets;
+        }
+
+        public void setNumberOfTickets(int numberOfTickets) {
+            this.NumberOfTickets = numberOfTickets;
+        }
+
+        public String getMovie() {
+            return Movie;
+        }
+
+        public void setMovie(String movie) {
+            this.Movie = movie;
+        }
+
+        public String getDateAndTime() {
+            return DateAndTime;
+        }
+
+        public void setDateAndTime(String dateAndTime) {
+            this.DateAndTime = dateAndTime;
+        }
+    }
+
+    //delete i update dugme u product and brand
+    @FXML
+    void deleteData(javafx.event.ActionEvent event) {
+        int selectedIndex = tableView.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            tableView.getItems().remove(selectedIndex);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            //prozor za ispisivanje poruke
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Data Selected");
+            alert.setContentText("Please select a data in the table.");
+            alert.showAndWait();
+        }
+    }
+
+
 }
