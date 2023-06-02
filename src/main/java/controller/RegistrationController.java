786ebc401566755fbe84652e676d09cce027748b
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-02 16:53:47 +0200

    making registration db and scene

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -1,5 +1,5 @@
 package controller;
-
+/*
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -31,7 +31,7 @@ public class RegistrationController {
     private TextField firstnameFieldRegistration;
     @FXML
     private Label errorLabelRegistration;
-
+*/
 
 /*
     @FXML
@@ -60,7 +60,7 @@ public class RegistrationController {
     }
 
  */
-
+/*
     @FXML
     void register(ActionEvent event) {
         String username = usernameFieldRegistration.getText();
@@ -91,6 +91,91 @@ public class RegistrationController {
 
         // Kreirajte korisnički račun
         boolean success = UserAccountService.createUserAccount(firstName, lastName, username, password);
+
+        if (success) {
+            errorLabelRegistration.setText("Registration successful!");
+            clearFields();
+        } else {
+            errorLabelRegistration.setText("Username already exists. Please choose a different username.");
+        }
+    }
+
+    private void clearFields() {
+        usernameFieldRegistration.clear();
+        passwordFieldRegistration.clear();
+        firstnameFieldRegistration.clear();
+        lastnameFieldRegistration.clear();
+    }
+
+}*/
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import java.io.IOException;
+import service.UserAccountService;
+
+
+public class RegistrationController {
+
+    @FXML
+    private TextField usernameFieldRegistration;
+
+    @FXML
+    private TextField passwordFieldRegistration;
+
+    @FXML
+    private TextField lastnameFieldRegistration;
+
+    @FXML
+    private TextField firstnameFieldRegistration;
+
+    @FXML
+    private Label errorLabelRegistration;
+
+    @FXML
+    private Button registration;
+    private UserAccountService userAccountService; // Dodano polje
+
+    public RegistrationController() {
+        userAccountService = new UserAccountService(); // Inicijalizacija objekta
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+        String username = usernameFieldRegistration.getText();
+        String password = passwordFieldRegistration.getText();
+        String firstName = firstnameFieldRegistration.getText();
+        String lastName = lastnameFieldRegistration.getText();
+
+        // Provjerite da li su sva polja popunjena
+        if (username.isEmpty() || password.isEmpty() ||  firstName.isEmpty() || lastName.isEmpty()) {
+            errorLabelRegistration.setText("Please fill in all fields");
+            return;
+        } else{
+            try {
+                // Učitavanje FXML datoteke drugog prozora
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainMenu.fxml"));
+                Parent root = loader.load();
+
+                // Otvaranje nove scene s drugim prozorom
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Kreirajte korisnički račun
+        boolean success = userAccountService.createUserAccount(firstName, lastName, username, password); // Promjena poziva metode
 
         if (success) {
             errorLabelRegistration.setText("Registration successful!");
