3f842f592967854a210e664e45cd468b1c8d232b
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-02 19:04:39 +0200

    Making tests

---
+++ src/test/java/controller/LogInControllerTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+public class LogInControllerTest {
+
+    @org.junit.Test
+    public void login() {
+    }
+
+    @org.junit.Test
+    public void validateLogin() {
+    }
+
+    @org.junit.Test
+    public void initialize() {
+    }
+}
