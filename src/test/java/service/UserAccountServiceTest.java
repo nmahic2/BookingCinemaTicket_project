42e34fde98d634597d926cc45a03ccdb21e9f1ba
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-05 16:42:12 +0200

    useraccount tests

---
+++ src/test/java/service/UserAccountServiceTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserAccountServiceTest {
+
+    @Test
+    public void createUserAccount() {
+    }
+
+    @Test
+    public void validateLogin() {
+    }
+}
