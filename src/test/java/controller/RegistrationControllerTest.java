500765b650dd99c7e3051322f415966d90375b5d
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-05 17:52:48 +0200

    error in controller tests

--- src/test/java/controller/RegistrationControllerTest.java
+++ src/test/java/controller/RegistrationControllerTest.java
@@ -1,10 +1,14 @@
 package controller;
-
+import javafx.scene.control.TextField;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class RegistrationControllerTest {
 
     @Test
     public void register() {
+        String username;
+        username = "nmahic2";
+        assertEquals("nmahic2", username);
     }
+
 }
