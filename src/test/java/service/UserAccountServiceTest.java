53760c821cca12bb4da31f5169c4662d9746e53a
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-05 17:52:29 +0200

    user account service tests

--- src/test/java/service/UserAccountServiceTest.java
+++ src/test/java/service/UserAccountServiceTest.java
@@ -1,5 +1,5 @@
 package service;
-
+/*
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +12,93 @@ public class UserAccountServiceTest {
 
     @Test
     public void validateLogin() {
+    }
+}*/
+import org.junit.Before;
+import org.junit.Test;
+import repository.UserAccountRepository;
+import service.UserAccountService;
+
+import static org.junit.Assert.*;
+
+public class UserAccountServiceTest {
+    private UserAccountRepository userRepository;
+    private UserAccountService userAccountService;
+
+    @Before
+    public void setup() {
+        userRepository = new UserAccountRepository();
+        userAccountService = new UserAccountService();
+    }
+/*
+    @Test
+    public void testCreateUserAccount_ValidData_ReturnsTrue() {
+        // Arrange
+        String firstName = "nejla";
+        String lastName = "mahic";
+        String username = "nmahic1";
+        String password = "12345";
+
+        // Act
+        boolean isCreated = UserAccountService.createUserAccount(firstName, lastName, username, password);
+
+// Assert
+        assertTrue(isCreated);
+
+    }
+*/
+    @Test
+    public void testCreateUserAccount_DuplicateUsername_ReturnsFalse() {
+        // Arrange
+        String firstName1 = "nejra";
+        String lastName1 = "mahic";
+        String username1 = "nmahic2";
+        String password1 = "123456";
+
+        String firstName2 = "nejla";
+        String lastName2 = "mahic";
+        String username2 = "nmahic2";
+        String password2 = "12345";
+
+        // Create first user account
+        userAccountService.createUserAccount(firstName1, lastName1, username1, password1);
+
+        // Act
+        boolean isCreated = userAccountService.createUserAccount(firstName2, lastName2, username2, password2);
+
+        // Assert
+        assertFalse(isCreated);
+    }
+
+    @Test
+    public void testValidateLogin_ValidCredentials_ReturnsTrue() {
+        // Arrange
+        String username = "nmahic2";
+        String password = "123456";
+
+        // Create user account
+        userAccountService.createUserAccount("nejra", "mahic", username, password);
+
+        // Act
+        boolean isValid = userAccountService.validateLogin(username, password);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_InvalidCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "johndoe";
+        String password = "wrongpassword";
+
+        // Create user account
+        userAccountService.createUserAccount("John", "Doe", username, "password");
+
+        // Act
+        boolean isValid = userAccountService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
     }
 }
