c90e1ad9fe94d15ba9960e5d315e89f91871035a
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-05 16:39:29 +0200

    login service updated

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -9,6 +9,10 @@ public class LoginService {
         this.loginRepo = new LoginDBRepository();
     }
 
+
+    public LoginService(LoginDBRepository loginRepo) {
+        this.loginRepo = loginRepo;
+    }
     public boolean validateLogin(String username, String password) {
         // Ovdje možete dodati dodatnu poslovnu logiku, provjeru i validaciju
         // npr. provjeru duljine korisničkog imena/lozinke, enkripciju itd.
