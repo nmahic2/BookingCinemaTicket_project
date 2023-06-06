fca93a94ae5b8b7b5af49ac5b8b1ba0b9ea33b60
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-06 14:28:16 +0200

    tests

---
+++ src/test/java/service/PriceDBServiceTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PriceDBServiceTest {
+
+    @Test
+    public void addData() {
+    }
+
+    @Test
+    public void getAllMovies() {
+    }
+
+    @Test
+    public void removeData() {
+    }
+}
