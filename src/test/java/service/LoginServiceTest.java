7a2a6d1d30332ebf9cdce99709c7b0d2d569fde3
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-08 19:20:29 +0200

    changes in tests using mock

--- src/test/java/service/LoginServiceTest.java
+++ src/test/java/service/LoginServiceTest.java
@@ -10,6 +10,7 @@ public class LoginServiceTest {
     public void validateLogin() {
     }
 }*/
+/*
 import org.junit.Before;
 import org.junit.Test;
 import repository.LoginDBRepository;
@@ -64,3 +65,63 @@ public class LoginServiceTest {
     }
 }
 
+*/
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import repository.LoginDBRepository;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class LoginServiceTest {
+    @Mock
+    private LoginDBRepository loginRepo;
+
+    private LoginService loginService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        loginService = new LoginService(loginRepo);
+    }
+
+    @Test
+    public void testValidateLogin_ValidCredentials_ReturnsTrue() {
+        // Arrange
+        String username = "nmahic2";
+        String password = "123456";
+        when(loginRepo.validateLogin(username, password)).thenReturn(true);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_InvalidCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "nmahic";
+        String password = "123456";
+        when(loginRepo.validateLogin(username, password)).thenReturn(false);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void testVslidateLogin_BlankSpace(){
+        String username = "";
+        String password = "";
+
+        boolean isBlank = loginService.validateLogin(username, password);
+
+    }
+}
