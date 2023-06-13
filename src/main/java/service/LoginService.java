226099dfbceda6c9bf99d086f468eef3c01080ec
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-13 19:42:57 +0200

    code reformat

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,29 +1,4 @@
-/*
-package service;
-import repository.LoginDBRepository;
-*/
-/*
-public class LoginService {
-    private LoginDBRepository loginRepo;
 
-    public LoginService() {
-        this.loginRepo = new LoginDBRepository();
-    }
-
-
-    public LoginService(LoginDBRepository loginRepo) {
-        this.loginRepo = loginRepo;
-    }
-    public boolean validateLogin(String username, String password) {
-        // Ovdje možete dodati dodatnu poslovnu logiku, provjeru i validaciju
-        // npr. provjeru duljine korisničkog imena/lozinke, enkripciju itd.
-
-        return loginRepo.validateLogin(username, password);
-    }
-
-
-}
-*/
 
 
 package service;
