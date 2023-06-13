e884dcd5c7f0a3e062d6aba9f2a528344db4298a
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-13 19:44:44 +0200

    code reformat

--- src/main/java/service/UserAccountService.java
+++ src/main/java/service/UserAccountService.java
@@ -1,30 +1,4 @@
 
-/*
-import repository.UserAccountRepository;
-
-public class UserAccountService {
-    private static UserAccountRepository userRepository;
-
-    public UserAccountService() {
-        this.userRepository = new UserAccountRepository();
-    }
-
-    public static boolean createUserAccount(String firstName, String lastName, String username, String password) {
-        // Provjera je li korisničko ime već zauzeto
-        if (userRepository.findByUsername(username)) {
-            return false;
-        }
-
-        userRepository.save(firstName, lastName, username, password);
-        return true;
-    }
-
-    public boolean validateLogin(String username, String password) {
-        return userRepository.validateLogin(username, password);
-    }
-}
-*/
-
 
 package service;
 import repository.UserAccountRepository;
