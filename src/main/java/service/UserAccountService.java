1106270b00d5b69e39c6b261feb2360792ae9066
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-13 16:51:32 +0200

    changes

--- src/main/java/service/UserAccountService.java
+++ src/main/java/service/UserAccountService.java
@@ -1,4 +1,4 @@
-package service;
+
 /*
 import repository.UserAccountRepository;
 
@@ -25,7 +25,7 @@ public class UserAccountService {
 }
 */
 
-
+/*
 import repository.UserAccountRepository;
 
 public class UserAccountService {
@@ -34,6 +34,54 @@ public class UserAccountService {
     public UserAccountService() {
         this.userRepository = new UserAccountRepository();
     }
+
+    public boolean createUserAccount(String firstName, String lastName, String username, String password) {
+        // Provjera je li korisničko ime već zauzeto
+        if (userRepository.findByUsername(username)) {
+            return false;
+        }
+
+        userRepository.save(firstName, lastName, username, password);
+        return true;
+    }
+
+    public boolean validateLogin(String username, String password) {
+        return userRepository.validateLogin(username, password);
+    }
+}*/
+/*import repository.UserAccountRepository;
+
+public class UserAccountService {
+    private UserAccountRepository userRepository;
+
+    public UserAccountService() {
+        this.userRepository = new UserAccountRepository();
+    }
+
+    public boolean createUserAccount(String firstName, String lastName, String username, String password) {
+        // Provjera je li korisničko ime već zauzeto
+        if (userRepository.findByUsername(username)) {
+            return false;
+        }
+
+        userRepository.save(firstName, lastName, username, password);
+        return true;
+    }
+
+    public boolean validateLogin(String username, String password) {
+        return userRepository.validateLogin(username, password);
+    }
+}*/
+package service;
+import repository.UserAccountRepository;
+
+public class UserAccountService {
+   private UserAccountRepository userRepository;
+
+   public UserAccountService() {
+        this.userRepository = new UserAccountRepository();
+    }
+
 
     public boolean createUserAccount(String firstName, String lastName, String username, String password) {
         // Provjera je li korisničko ime već zauzeto
