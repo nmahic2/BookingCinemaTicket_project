e90294da33bd0eb6a50ddd29dc77c178b9585e86
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-23 14:27:58 +0200

    javadoc komentari

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -2,18 +2,34 @@
 package service;
 
 import repository.LoginDBRepository;
-
+/**
+ * Servis za provjeru valjanosti prijave korisnika.
+ */
 public class LoginService {
     private LoginDBRepository loginRepo;
-
+    /**
+     * Konstruktor koji stvara novu instancu servisa s podrazumijevanim repozitorijem.
+     */
     public LoginService() {
         this.loginRepo = new LoginDBRepository();
     }
-
+    /**
+     * Konstruktor koji stvara novu instancu servisa s određenim repozitorijem.
+     *
+     * @param loginRepo Repozitorij za prijavu korisnika
+     */
 
     public LoginService(LoginDBRepository loginRepo) {
         this.loginRepo = loginRepo;
     }
+
+    /**
+     * Provjerava valjanost korisničkog prijavljivanja.
+     *
+     * @param username Korisničko ime
+     * @param password Lozinka
+     * @return {@code true} ako je prijava valjana, inače {@code false}
+     */
     public boolean validateLogin(String username, String password) {
         // Ovdje možete dodati dodatnu poslovnu logiku, provjeru i validaciju
         // npr. provjeru duljine korisničkog imena/lozinke, enkripciju itd.
