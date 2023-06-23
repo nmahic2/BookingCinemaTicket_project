ffa060f8e304656fe0fc6407dea6770eab3ad883
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-23 14:30:00 +0200

    javadoc komentari

--- src/main/java/service/PriceDBService.java
+++ src/main/java/service/PriceDBService.java
@@ -7,26 +7,46 @@ import repository.PriceDBRepository;
 
 import java.util.List;
 
-
+/**
+ * Servis za upravljanje podacima vezanim uz cijene i filmove.
+ */
 public class PriceDBService {
     private PriceDBRepository priceDBRepository;
-
+    /**
+     * Konstruktor koji stvara novu instancu servisa s podrazumijevanim repozitorijem.
+     */
     public PriceDBService() {
         priceDBRepository = new PriceDBRepository();
     }
-
+    /**
+     * Konstruktor koji stvara novu instancu servisa s određenim repozitorijem.
+     *
+     * @param priceDBRepository Repozitorij za cijene i filmove
+     */
     public PriceDBService(PriceDBRepository priceDBRepository) {
         this.priceDBRepository = priceDBRepository;
     }
-
+    /**
+     * Dodaje podatke o kartici u repozitorij.
+     *
+     * @param data Podaci o kartici
+     */
     public void addData(CartController.Data data) {
         priceDBRepository.addData(data);
     }
-
+    /**
+     * Dohvaća sve filmove iz repozitorija.
+     *
+     * @return Lista svih filmova
+     */
     public List<String> getAllMovies() {
         return priceDBRepository.getAllMovies();
     }
-
+    /**
+     * Uklanja podatke o kartici iz repozitorija.
+     *
+     * @param data Podaci o kartici za uklanjanje
+     */
     public void removeData(CartController.Data data) {
         priceDBRepository.removeData(data);
     }
