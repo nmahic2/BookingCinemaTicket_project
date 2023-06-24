54b409cde882e6b37c491c8a3e098204f2c5d04e
Nejra Mahić <nmahic2@etf.unsa.ba>
2023-06-24 15:51:25 +0200

    reformat code

--- src/main/java/service/UserAccountService.java
+++ src/main/java/service/UserAccountService.java
@@ -1,24 +1,28 @@
 
 package service;
+
 import repository.UserAccountRepository;
+
 /**
  * Servis za upravljanje korisničkim računima i autentikacijom.
  */
 public class UserAccountService {
-   private UserAccountRepository userRepository;
+    private UserAccountRepository userRepository;
+
     /**
      * Konstruktor koji stvara novu instancu servisa s podrazumijevanim repozitorijem.
      */
     public UserAccountService() {
         this.userRepository = new UserAccountRepository();
     }
+
     /**
      * Kreira korisnički račun s navedenim podacima.
      *
      * @param firstName Ime korisnika
-     * @param lastName Prezime korisnika
-     * @param username Korisničko ime
-     * @param password Lozinka
+     * @param lastName  Prezime korisnika
+     * @param username  Korisničko ime
+     * @param password  Lozinka
      * @return true ako je korisnički račun uspješno kreiran, false ako je korisničko ime već zauzeto
      */
 
